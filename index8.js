bf4bebcdef184872e8fdcb3b56059eaaf14f4c0d
arinn3310 <arinupadhyay10@gmail.com>
2024-08-14 10:07:13 +0530

    conditional statement

---
+++ index8.js
@@ -0,0 +1,145 @@
+// console.log("hello techno");
+
+// alert("hello techno");
+
+// var let const all are keywords 
+// whatt is the diff btw them
+
+
+// var firstNum = 20;
+
+// console.log(firstNum);
+
+// datatypes;;;;primitive types and reference or object typed 
+// primitive types- string , number, boolean , undefined , symbol , bigint;
+// // reference types- array , object , function , date , null ;
+// var firstvar = "hello techno";
+// firstvar = [20,30];
+// var firstvar = {
+//     firstname : "arin",
+//     lastname : "upadhyay"
+//     };
+//     firstvar = true;
+//     console.log(firstvar);
+
+
+    // [{},{},{}]-json
+    // {key : value}-javascript object
+
+
+// var let const all are keywords 
+// whatt is the diff btw them// var num1 = 20;
+ // console.log("Hello,techno");
+ // alert("Hello techno");
+ // var,let,const
+ // var firstnum =20;
+ // console.log(firstnum);
+ // primitive types --> string,number,boolean,undefined,symbol,bigint....
+ // reference or object typed data types --> array , object, date,
+ 
+ // var firstvar = "i'm a full stack developer";
+ // console.log(firstvar);
+ // console.log(typeof firstvar);
+ 
+ // var firstvar = false;
+ // console.log(firstvar);
+ // console.log(typeof firstvar);
+ 
+ 
+ // var firstvar = undefined;
+ // console.log(firstvar);
+ // console.log(typeof firstvar);
+ 
+ // var firstvar = undefined;
+ // console.log(firstvar);
+ // console.log(typeof firstvar);
+ 
+ // var firstarray = [1,2,3,"hello",true,undefined];
+ // console.log(firstarray);
+ // console.log(typeof firstarray);
+ 
+ // var date = new Date();
+ // console.log(date);
+ // console.log(typeof date);
+ 
+ // var firstvar = "Hello techno";
+ // firstvar = [20,30];
+ // firstvar = {
+ //         firstname : "hemant",
+ //         lastname : "ahuja"
+ // };
+ // firstvar = true;
+ 
+ // console.log(firstvar);
+ 
+ 
+ // var num1 = {
+ //      firstname = "hemant";
+ // }
+ // console.log(num1);
+ 
+ // let num2 = 20;
+ 
+ // var mum1 = 20;
+ 
+ // var num1 = 30;
+ //  variable define with var can be redeclared or reassingned 
+ 
+ // let num2 = 40;
+ //  num2 = 100;
+ // variables defined woth let cannot be redeclared but can be reassigned
+ 
+ // const num3 = 100;
+ // num3 = 1000;
+ // variables defined with const cannot be redeclared or reassigned
+ 
+ 
+ // operators --> 
+ 
+ // arithmetic operators
+ // + , - , * , /
+ 
+ // let a=10;
+ // let b=20;
+ 
+ // let add = a+b;
+ // let sub = a-b;
+ // let mul = a*b;
+ // let div = a/b;
+ // console.log(add);
+ // console.log(sub);
+ // console.log(mul);
+ // console.log(div);
+ 
+ // let firstname="arin";
+ // let lastname="don";
+ 
+ // console.log(firstname+""+lastname);
+ 
+ // console.log(a**b);
+ // console.log(a%b);
+ 
+ // console.log(5*"5");
+ // console.log("5"/5);
+ 
+ //addition assignments
+ 
+ //console.log("attif"-10); 
+ 
+ // let str1 ="5";
+ // let str2 ="+str1";
+ 
+ //console.log(5+"5");
+ 
+
+ //y-=3;
+ //y*=3;
+ //y/=3;
+ //y%=3;
+ //y**=3;
+ let y=10;
+ console.log(y)
+
+let num4 = 10;
+let num5 = 20;
+console.log(num4 == num5);
---
+++ index9.js
@@ -0,0 +1,57 @@
+// conditional statement
+// if else,switch,ternary operators
+// functions
+// array,objects
+// loops
+
+// let count = 20;
+
+
+//  if (count==20){
+
+//     console.log("the count is 20");
+//  }else{
+
+//     console.log("the count is not 20");
+//  }
+
+// if(count==20){
+//     console.log("the count is 20");
+// }else if(count > 20){
+//     console.log("the count is greater than 20");
+// }else{
+//     console.log("the count is less than 20");
+// }
+
+
+// // for taking input from user we use promt
+
+// let firstnumber = prompt("enter the first number");
+
+// console.log(firstnumber);
+
+
+// promt the user to enter their username and Pass
+// if username is admin and password is 123 in numbers
+// if both connection is true then display auth successfull else auth failed
+
+
+//   let username = prompt("Enter your username:");
+//  let password = prompt("Enter your password:");
+
+// //   if (username === "admin" && password === "123") {
+// //     alert("Auth successful!");
+
+// //   }else{
+// //     alert("Auth failed! Try again.");
+// //   
+
+// if(username === "admin"){
+
+// }
+
+// ternary operator
+
+let COUNT = 20;
+
+COUNT=== 20 ? console.log("the count is 20") : COUNT > 20 ? console.log("the count is grtr then 20") : console.log("the count is less then 20");
